f94b37ae8b783888aef0d3fb0490d61d42da4ce9
Mehmet Ersan UZUN <mersanuzun@gmail.com>
2016-12-21 14:20:02 +0200

    Update Request.js

--- Request.js
+++ Request.js
@@ -83,18 +83,3 @@ function Request(url){
 
 	return api;
 }
-
-
-
-Request("http://jsonplaceholder.typicode.com").path(["comments"]).get().then((r) => {console.log(r)})
-
-Request("http://localhost:3000").path(["postResult"]).data({name: "ersan"}).post().then((result) => {
-	console.log(result); 
-	Request("http://jsonplaceholder.typicode.com/albums").get().then((sResult) => {
-		console.log("içerde" + sResult);
-	}).catch((err) => {
-		console.log("içerde " + err);
-	});
-}).catch((err) => {
-	console.log(err)
-});
